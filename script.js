35108061e95d1d1c56fa1167f0766c0babe5ac79
Talha-Rasool <talharasool450@gmail.com>
2024-06-08 16:52:36 +0200

    `feat`:for loops useCase.

--- script.js
+++ script.js
@@ -2,13 +2,18 @@
 
 //loops
 
-for(let i = 0; i <=10; i++){
-    console.log(`lifting the weights ${i} times`)
+for (let i = 0; i <= 10; i++) {
+  console.log(`lifting the weights ${i} times`);
 }
 
- 
+const talha = ['talha', 'Augsburg', 23, 'Vollyball', true];
 
+const newTalha = [];
 
-
-
-
+for (let i = 0; i < talha.length; i++) {
+  if (typeof talha[i] !== 'string') continue;
+  if (typeof talha[i] === 'number') break;
+  console.log(talha[i], typeof talha[i]);
+  newTalha.push(typeof talha[i]);
+}
+console.log(newTalha);
