09da8f0a659b9690b40d91346305b9c50bbd66a1
Talha-Rasool <talharasool450@gmail.com>
2024-06-09 21:49:53 +0200

    `feat`:Destruccture Array;

--- script.js
+++ script.js
@@ -52,22 +52,67 @@ console.log(percentageOfWorld(population));
 ///loop in loop
 
 for (let p = 0; p <= 1; p++) {
-  console.log('parent  has costed his vote!');
+  //console.log('parent  has costed his vote!');
 
   for (let childs = 0; childs <= 3; childs++) {
-    console.log('child  have also casted his Vote');
+    //console.log('child  have also casted his Vote');
   }
 }
 
+const listOfNeighbours = [
+  ['Canada', 'Mexico'],
+  ['Spain'],
+  ['Norway', 'Sweden', 'Russia'],
+];
 
-const listOfNeighbours=[['Canada','Mexico'],['Spain'],['Norway','Sweden','Russia']];
+for (let i = 0; i < listOfNeighbours.length; i++) {
+  //console.log(listOfNeighbours[i])
 
-for(let i=0;i<listOfNeighbours.length;i++){
-    console.log(listOfNeighbours[i])
-
-    for(let y=0; y < listOfNeighbours[i].length; y++){
-        console.log(`Neighbour ${listOfNeighbours[i][y]}`)
-    }
+  for (let y = 0; y < listOfNeighbours[i].length; y++) {
+    //console.log(`Neighbour ${listOfNeighbours[i][y]}`)
+  }
 }
+
+/////Tip Calcution for a resturant//////
+
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totalAmount = [];
+
+function calcTip(bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.12;
+}
+
+console.log(calcTip(56));
+
+for (let i = 0; i < bills.length; i++) {
+  const tip = calcTip(bills[i]);
+  tips.push(tip);
+  const total = tip + bills[i];
+  totalAmount.push(total);
+  
+}
+console.log(totalAmount,tips)
+
+function calcAvg(arr){
+    let sum=0;
+    for(let i=0;i<arr.length;i++){
+        sum=+arr[i];
+    }
+    return sum/arr.length;
+}
+
+calcAvg(bills)
+
+///Destructring arrays
+
+const ratings = [['rating', 4.19], ['ratingsCount', 144584]];
+
+const[[,rating],[,ratingCount]]=ratings;
+console.log(rating,ratingCount)
+
+const ratingStars = [63405, 1808];
+const[fiveStarRatings=0,oneStarRatings=0,threeStarRatings=0]=ratingStars;
+console.log(fiveStarRatings,oneStarRatings,threeStarRatings)
 
 
