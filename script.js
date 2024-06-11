97046130d574818296b624c0a9bd8ce55e72a578
Talha-Rasool <talharasool450@gmail.com>
2024-06-11 23:53:32 +0200

    `refactor`:refactor some code for practice.

--- script.js
+++ script.js
@@ -135,7 +135,7 @@ const resturant = {
       close: 22,
     },
   },
-  orderFood: function ({ addres, time = 20, starterIndex, mainIndex = 1 }) {
+  orderFood: function ({ addres, time = 20, starterIndex=2, mainIndex = 1 }) {
     console.log(
       `your order is delieveed to ${addres} will arrive at ${time} includes ${this.starterMenue[starterIndex]} and ${this.mainMenue[mainIndex]}`
     );
@@ -182,11 +182,24 @@ newResturant.name = 'burger king';
 console.log(resturant);
 console.log(newResturant);
 
+
+///for of loop in array[value,key,entries]//////
 const finalMenue=[...resturant.starterMenue,...resturant.mainMenue];
 for(const [i,el] of finalMenue.entries()) {
     console.log(`${i+1}: ${el}`);
 }
 console.log(finalMenue);
+
+console.log('----------for of loop on objects-----------------');
+
+for(const [day,{open,close}] of Object.entries(resturant.openingHour)) {
+    console.log(`we are open on ${day} at ${open} and will close at ${close}`)
+}
+
+
+//////optional chaining/////////////////
+
+resturant.orderFood?.({addres:'ali baba chowk'}) ?? 'this not exist';
 
 ////rest pattern
 
@@ -242,27 +255,26 @@ console.log(rest1,rest2);
 /////Assignment /////
 
 /* 
-We're building a football betting app (soccer for my American friends 😅)!
+Let's continue with our football betting app!
 
-Suppose we get data from a web service about a certain game (below). In this challenge we're gonna work with the data. So here are your tasks:
+1. Loop over the game.scored array and print each player name to the console, along with the goal
+ number (Example: "Goal 1: Lewandowski")
+2. Use a loop to calculate the average odd and log it to the console (We already studied how to 
+calculate averages, you can go check if you don't remember)
+3. Print the 3 odds to the console, but in a nice formatted way, exaclty like this:
+      Odd of victory Bayern Munich: 1.33
+      Odd of draw: 3.25
+      Odd of victory Borrussia Dortmund: 6.5
+Get the team names directly from the game object, don't hardcode them (except for "draw"). HINT:
+ Note how the odds and the game objects have the same property names 😉
 
-1. Create one player array for each team (variables 'players1' and 'players2')
-2. The first player in any player array is the goalkeeper and the others are field players. 
-For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array 
-('fieldPlayers') with all the remaining 10 field players
-3. Create an array 'allPlayers' containing all players of both teams (22 players)
-4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array 
-('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
-5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and
- 'team2')
-6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array) 
-and prints each of them to the console, along with the number of goals that were scored in total 
-(number of player names passed in)
-7. The team with the lower odd is more likely to win. Print to the console which team is more likely 
-to win, WITHOUT using an if/else statement or the ternary operator.
-
-TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then, call the function
- again with players from game.scored
+BONUS: Create an object called 'scorers' which contains the names of the players who scored as
+ properties, and the number of goals as the value. In this game, it will look like this:
+      {
+        Gnarby: 1,
+        Hummels: 1,
+        Lewandowski: 2
+      }
 
 GOOD LUCK 😀
 */
