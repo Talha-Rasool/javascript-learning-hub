ebced22cdfd0fd9ef9b7d353a55c5c715e1b18a1
Talha-Rasool <talharasool450@gmail.com>
2024-06-14 18:16:03 +0200

    `feat`:use array and function to calculate values.

--- script.js
+++ script.js
@@ -17,10 +17,36 @@ for (let i = 0; i < talha.length; i++) {
 }
 
 //break
-console.log('-------break------')
-for(let i=0; i<talha.length;i++){
-    if(typeof talha[i]==='number') break;
-    console.log(talha[i])
-
+console.log('-------break------');
+for (let i = 0; i < talha.length; i++) {
+  if (typeof talha[i] === 'number') break;
+  console.log(talha[i]);
 }
 console.log(newTalha);
+//election assignment
+
+for (let i = 0; i < 50; i++) {
+  console.log(`Voter number 1 is currently voting`);
+}
+
+//////////
+
+const population = [24, 8, 202, 1441];
+if (population.length === 4) {
+    console.log(true);
+  } else {
+    console.log(false);
+  }
+
+function percentageOfWorld(pop) {
+  const population = [];
+  for (let i = 0; i < pop.length; i++) {
+    const ppl = (pop[i] / 7900) * 100;
+    population.push(ppl);
+  }
+  return population;
+}
+
+console.log(percentageOfWorld(population));
+
+
